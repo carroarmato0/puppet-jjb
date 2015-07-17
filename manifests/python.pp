0ef8f1ddbf77b44fde6193f34b794255a7c50652
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-07-17 10:29:26 +0200

    Specify specific jenkins-job-builder repository containing a patch
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/python.pp
+++ manifests/python.pp
@@ -15,6 +15,7 @@ class jjb::python {
 
   python::pip { 'jenkins-job-builder' :
     pkgname  => 'jenkins-job-builder',
+    url      => 'git+https://github.com/carroarmato0/jenkins-job-builder.git#egg=python-jenkins-job-builder',
   }
 
 }
