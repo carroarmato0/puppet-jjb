9070c3dd395451b13905a9c2b76ef3139ba99ef1
Christophe Vanlancker <carroarmato0@gmail.com>
2015-10-27 12:36:00 +0100

    Have python::pip install jenkins-job-builder from upstream

--- manifests/python.pp
+++ manifests/python.pp
@@ -15,7 +15,6 @@ class jjb::python {
 
   python::pip { 'jenkins-job-builder' :
     pkgname => 'jenkins-job-builder',
-    url     => 'git+https://github.com/carroarmato0/jenkins-job-builder.git#egg=python-jenkins-job-builder',
   }
 
 }
